72d9077821e15ba5590f01409ebcc07b60789b77
MinhPhucHCMUAF <22130215@st.hcmuaf.edu.vn>
2026-06-03 10:06:02 +0700

    tuan 1- thay doi header components

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,32 +2,34 @@ import { Stack, Tabs } from "expo-router";
 import "./global.css";
 // import { View, Text } from "react-native";
 import {Ionicons} from "@expo/vector-icons";
+import Header from "@/components/Header";
 export default function RootLayout() {
   // @ts-ignore
   return (
       <Tabs
           screenOptions={{
-            headerShown: false,
-            tabBarActiveTintColor: "#d97706",
-            tabBarInactiveTintColor: "#000000",
+              headerShown: true,
+              header: () => <Header />,
+              tabBarActiveTintColor: "#d97706",
+              tabBarInactiveTintColor: "#000000",
 
-            tabBarStyle: {
-              height: 80,
-              borderRadius: 30,
-              marginHorizontal: 10,
-              marginBottom: 10,
-              position: "absolute",
-              backgroundColor: "#F5EDED",
-              borderTopWidth: 0,
-              bottom: 70,
-              paddingTop: 5,
-              paddingBottom: 8,
+              tabBarStyle: {
+                  height: 80,
+                  borderRadius: 30,
+                  marginHorizontal: 10,
+                  marginBottom: 10,
+                  position: "absolute",
+                  backgroundColor: "#F5EDED",
+                  borderTopWidth: 0,
+                  bottom: 70,
+                  paddingTop: 5,
+                  paddingBottom: 8,
             },
 
-            tabBarLabelStyle: {
-              fontSize: 11,
-              textAlign: "center",
-              marginBottom: 5,
+              tabBarLabelStyle: {
+                  fontSize: 11,
+                  textAlign: "center",
+                  marginBottom: 5,
             },
           }}
       >
--- app/main/index.tsx
+++ app/main/index.tsx
@@ -1,10 +1,14 @@
-import { Text, View } from "react-native";
+import {Animated, Text, TouchableOpacity, View} from "react-native";
 import '../global.css';
+import ScrollView = Animated.ScrollView;
+import {Image} from "expo-image";
+import {Ionicons} from "@expo/vector-icons";
+import { Stack } from "expo-router";
 
 export default function Index() {
-  return (
-    <View className="flex-1">
-        <Text>WELCOME</Text>
-    </View>
+    return (
+        <ScrollView style={{ flex: 1, backgroundColor: "#F5EDED" }} showsVerticalScrollIndicator={false}>
+
+        </ScrollView>
   );
 }
