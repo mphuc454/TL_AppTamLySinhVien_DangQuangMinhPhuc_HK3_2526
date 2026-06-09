c2963610d0f09b12438d7ee36972f2b57e8e634e
MinhPhucHCMUAF <22130215@st.hcmuaf.edu.vn>
2026-06-09 17:57:59 +0700

    tuan 2.ver.1 - them layout chatbot va dich vu

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,5 @@
 import { Ionicons } from "@expo/vector-icons";
-import { Animated, FlatList, Text, TouchableOpacity, View } from "react-native";
+import { Animated, FlatList, Text, TouchableOpacity, View, Button } from "react-native";
 import "./global.css";
 import ScrollView = Animated.ScrollView;
 
@@ -10,16 +10,24 @@ const Data = [
   { id: 4, name: "Giận dữ", icon: "thunderstorm-outline", bg: "#F09E54" },
   { id: 5, name: "Buồn bã", icon: "sad-outline", bg: "#C3F2A6" },
 ];
+const Data2 = [
+  {id: 1, name: "Nhật ký", icon:"book", bg:"#F9EED8"},
+  {id: 2, name: "Đặt lịch hẹn", icon:"calendar", bg:"#F9EED8"},
+  {id: 3, name: "Kiểm tra tâm trạng", icon:"heart", bg:"#F9EED8"},
+  {id: 4, name: "Bài khảo sát", icon:"checkbox", bg:"#F9EED8"}
+]
 export default function Index() {
   return (
     <ScrollView
       style={{ flex: 1, backgroundColor: "#F5EDED" }}
       showsVerticalScrollIndicator={false}
     >
+      {/* Layout1: Lời chào mở đầu */}
       <View style={{ flex: 1, paddingHorizontal: 20, paddingTop: 20 }}>
         <Text style={{ fontSize: 18, fontWeight: "bold" }}>
           Chào bạn, User! Bạn cảm thấy thế nào !
         </Text>
+        {/* Layout2: Hỏi tâm trạng ntn */}
         <Text
           style={{
             fontSize: 16,
@@ -63,6 +71,59 @@ export default function Index() {
             </TouchableOpacity>
           )}
         ></FlatList>
+        {/* Layout3: Chatbot AI */}
+        <View style={{ marginTop: 30, backgroundColor: "#604FD9", flexDirection: "row", alignItems: "center", borderRadius: 30, padding: 10, width: '100%' }}>
+          {/* cột 1: icon */}
+          <View style={{ flex: 1, alignItems: "center" }}>
+            <Ionicons name="chatbubble" size={52} color="#FFF0F0" />
+          </View>
+          {/* cột 2: text */}
+          <View style={{ flex: 3 }}>
+            <Text style={{ fontSize: 16, fontWeight: "bold", color: "#FFF0F0" }}>
+              Trò chuyện với AI
+            </Text>
+            <Text style={{ fontSize: 12, fontWeight: "medium", color: "#FFF0F0", padding: 10 }}>
+              Lắng nghe & hỗ trợ 24/7
+            </Text>
+          </View>
+          {/* cột 3: nút */}
+          <View style={{ flex: 2 }}>
+            <Button title="Bắt đầu" onPress={() => console.log('Button with adjusted color pressed')}></Button>
+          </View>
+        </View>
+        {/* Layout4: Các dịch vụ */}
+        <FlatList data={Data2}
+                  numColumns={2}
+                  columnWrapperStyle={{justifyContent: "space-between", marginBottom: 20}}
+                  keyExtractor={(item) => item.id.toString()}
+                  renderItem={({item}) =>(
+                    <TouchableOpacity style={{ alignItems: "center", marginRight: 2}}>
+                      <View
+                        style={{
+                          width: 165.02,
+                          height: 62,
+                          backgroundColor: item.bg,
+                          borderRadius: 16,
+                          marginTop: 20,
+                          flexDirection:"row",
+                          alignItems: "center",
+                          paddingHorizontal: 10,
+                          borderWidth: 1, 
+                          borderColor: "#D8AD93"
+                        }}
+                      >
+                        <Ionicons name={item.icon as any} size={25} color="#D8AD93" />
+                        <Text
+                        style={{fontSize: 10, fontWeight: "bold", marginLeft: 10, color: "#573926", flex: 1}}
+                      >
+                        {item.name}
+                      </Text>
+                      </View>
+                    </TouchableOpacity>
+                  )}
+        >
+        </FlatList>
+        {/* Layout5: Các bài tập */}
       </View>
     </ScrollView>
   );
