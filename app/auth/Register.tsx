613ae4870bea99d3e44a7b0d69ded036b5fd4770
MinhPhucHCMUAF <22130215@st.hcmuaf.edu.vn>
2026-06-20 11:07:43 +0700

    tuan 3.ver.5 - add setting.tsx language.tsx theme.tsx for user

--- app/auth/Register.tsx
+++ app/auth/Register.tsx
@@ -12,162 +12,203 @@ export default function Register() {
         paddingHorizontal: 20,
       }}
     >
-     <View 
-      style={{ flexDirection: "row", alignItems: "center", marginTop: 30 }}
-     >
-      <TouchableOpacity onPress={() => router.push("/")}>
-                <Ionicons name="arrow-back" size={25}></Ionicons>
-              </TouchableOpacity>
-      <Text style={{
-          flex: 1,
+      <View
+        style={{ flexDirection: "row", alignItems: "center", marginTop: 30 }}
+      >
+        <TouchableOpacity onPress={() => router.push("/")}>
+          <Ionicons name="arrow-back" size={25}></Ionicons>
+        </TouchableOpacity>
+        <Text
+          style={{
+            flex: 1,
             textAlign: "center",
             fontSize: 20,
             fontWeight: "bold",
-      }}>Tạo tài khoản</Text>
-     </View>
-      <View style={{
-                  backgroundColor: "#FFF",
-                  borderWidth: 1,
-                  borderColor: "#E5E5E5",
-                  borderRadius: 10,
-                  height: 52,
-                  paddingHorizontal: 12,
-                  marginTop: 30
-            }}>
-            <TextInput style={{ flex: 1}} placeholder="Nhập tên của bạn"></TextInput>
-            </View>
-             <View style={{
-                  backgroundColor: "#FFF",
-                  borderWidth: 1,
-                  borderColor: "#E5E5E5",
-                  borderRadius: 10,
-                  height: 52,
-                  paddingHorizontal: 12,
-                  marginTop: 15
-            }}>
-            <TextInput style={{ flex: 1}} placeholder="Nhập email của bạn"></TextInput>
-            </View>
-             <View style={{
-                  backgroundColor: "#FFF",
-                  borderWidth: 1,
-                  borderColor: "#E5E5E5",
-                  borderRadius: 10,
-                  height: 52,
-                  paddingHorizontal: 12,
-                  marginTop: 15
-            }}>
-            <TextInput style={{ flex: 1}} placeholder="Nhập số điện thoại của bạn"></TextInput>
-            </View>
-             <View style={{
-                  flexDirection: "row",
-                  alignItems: "center",
-                  backgroundColor: "#FFF",
-                  borderWidth: 1,
-                  borderColor: "#E5E5E5",
-                  borderRadius: 10,
-                  height: 52,
-                  paddingHorizontal: 12,
-                  marginTop: 15
-            }}>
-            <TextInput style={{ flex: 1}} placeholder="Nhập mật khẩu của bạn"></TextInput>
-            <TouchableOpacity>
-              <Feather name={"eye"} size={20}></Feather>
-            </TouchableOpacity>
-            </View>
-            <View style={{
-                  marginTop: 15
-            }}>
-              <Text style={{fontSize: 12, marginBottom: 10}}>Xác nhận mật khẩu</Text>
-            <TextInput style={{ 
-              backgroundColor: "#FFF",
-                  borderWidth: 1,
-                  borderColor: "#E5E5E5",
-                  borderRadius: 10,
-                  height: 52,
-                  paddingHorizontal: 12}}></TextInput>
-            </View>
-            <TouchableOpacity
-                      style={{
-                        marginTop: 15,
-                        backgroundColor: "#6D00D9",
-                        height: 60,
-                        width: "100%",
-                        borderRadius: 16,
-                        justifyContent: "center",
-                        alignItems: "center",
-                        elevation: 3,
-                      }}
-                    >
-                      <Text
-                        style={{
-                          color: "#fff",
-                          fontSize: 18,
-                          fontWeight: "700",
-                        }}
-                      >
-                       Đăng nhập
-                      </Text>
-                    </TouchableOpacity>
-                    <Text
-                    style={{
-                          textAlign: "center",
-                          marginTop: 10,
-                          color: "#777",
-                          fontSize: 18}}
-                            >
-                              or
-                      </Text>
-                      <TouchableOpacity
-                               style={{
-                                 marginTop: 10,
-                                 height: 60,
-                                 width: "100%",
-                                 borderRadius: 16,
-                                 justifyContent: "center",
-                                 alignItems: "center",
-                                 borderWidth: 1,
-                                 flexDirection: "row",
-                                 gap: 10,
-                               }}
-                             >
-                               <Ionicons name="logo-facebook" size={24} color="#1877F2" />
-                               <Text>Tiếp tục với Facebook</Text>
-                             </TouchableOpacity>
-                             <TouchableOpacity
-                                       style={{
-                                         marginTop: 10,
-                                         height: 60,
-                                         width: "100%",
-                                         borderRadius: 16,
-                                         justifyContent: "center",
-                                         alignItems: "center",
-                                         borderWidth: 1,
-                                         flexDirection: "row",
-                                         gap: 10,
-                                       }}
-                                     >
-                                       <Ionicons name="logo-google" size={24} color="#EA4335" />
-                                       <Text>Tiếp tục với Google</Text>
-                              </TouchableOpacity>
-                              <View style={{
-                                      flexDirection: "row",
-                                      justifyContent: "center",
-                                      marginTop: 30,
-                                    }}>
-                                      <Text style={{
-                                         color: "#555",
-                                         fontSize: 13,
-                                      }}>
-                                        Bạn đã có tài khoản?
-                                      </Text>
-                                      <TouchableOpacity onPress={() => router.push("/login")}>
-                                        <Text style={{
-                                          color: "#4169E1",
-                                          fontWeight: "bold",
-                                          fontSize: 13
-                                        }}> Đăng nhập</Text>
-                                      </TouchableOpacity>
-                                    </View> 
+          }}
+        >
+          Tạo tài khoản
+        </Text>
+      </View>
+      <View
+        style={{
+          backgroundColor: "#FFF",
+          borderWidth: 1,
+          borderColor: "#E5E5E5",
+          borderRadius: 10,
+          height: 52,
+          paddingHorizontal: 12,
+          marginTop: 30,
+        }}
+      >
+        <TextInput
+          style={{ flex: 1 }}
+          placeholder="Nhập tên của bạn"
+        ></TextInput>
+      </View>
+      <View
+        style={{
+          backgroundColor: "#FFF",
+          borderWidth: 1,
+          borderColor: "#E5E5E5",
+          borderRadius: 10,
+          height: 52,
+          paddingHorizontal: 12,
+          marginTop: 15,
+        }}
+      >
+        <TextInput
+          style={{ flex: 1 }}
+          placeholder="Nhập email của bạn"
+        ></TextInput>
+      </View>
+      <View
+        style={{
+          backgroundColor: "#FFF",
+          borderWidth: 1,
+          borderColor: "#E5E5E5",
+          borderRadius: 10,
+          height: 52,
+          paddingHorizontal: 12,
+          marginTop: 15,
+        }}
+      >
+        <TextInput
+          style={{ flex: 1 }}
+          placeholder="Nhập số điện thoại của bạn"
+        ></TextInput>
+      </View>
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          backgroundColor: "#FFF",
+          borderWidth: 1,
+          borderColor: "#E5E5E5",
+          borderRadius: 10,
+          height: 52,
+          paddingHorizontal: 12,
+          marginTop: 15,
+        }}
+      >
+        <TextInput
+          style={{ flex: 1 }}
+          placeholder="Nhập mật khẩu của bạn"
+        ></TextInput>
+        <TouchableOpacity>
+          <Feather name={"eye"} size={20}></Feather>
+        </TouchableOpacity>
+      </View>
+      <View
+        style={{
+          marginTop: 15,
+        }}
+      >
+        <Text style={{ fontSize: 12, marginBottom: 10 }}>
+          Xác nhận mật khẩu
+        </Text>
+        <TextInput
+          style={{
+            backgroundColor: "#FFF",
+            borderWidth: 1,
+            borderColor: "#E5E5E5",
+            borderRadius: 10,
+            height: 52,
+            paddingHorizontal: 12,
+          }}
+        ></TextInput>
+      </View>
+      <TouchableOpacity
+        style={{
+          marginTop: 15,
+          backgroundColor: "#6D00D9",
+          height: 60,
+          width: "100%",
+          borderRadius: 16,
+          justifyContent: "center",
+          alignItems: "center",
+          elevation: 3,
+        }}
+      >
+        <Text
+          style={{
+            color: "#fff",
+            fontSize: 18,
+            fontWeight: "700",
+          }}
+        >
+          Đăng nhập
+        </Text>
+      </TouchableOpacity>
+      <Text
+        style={{
+          textAlign: "center",
+          marginTop: 10,
+          color: "#777",
+          fontSize: 18,
+        }}
+      >
+        or
+      </Text>
+      <TouchableOpacity
+        style={{
+          marginTop: 10,
+          height: 60,
+          width: "100%",
+          borderRadius: 16,
+          justifyContent: "center",
+          alignItems: "center",
+          borderWidth: 1,
+          flexDirection: "row",
+          gap: 10,
+        }}
+      >
+        <Ionicons name="logo-facebook" size={24} color="#1877F2" />
+        <Text>Tiếp tục với Facebook</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={{
+          marginTop: 10,
+          height: 60,
+          width: "100%",
+          borderRadius: 16,
+          justifyContent: "center",
+          alignItems: "center",
+          borderWidth: 1,
+          flexDirection: "row",
+          gap: 10,
+        }}
+      >
+        <Ionicons name="logo-google" size={24} color="#EA4335" />
+        <Text>Tiếp tục với Google</Text>
+      </TouchableOpacity>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "center",
+          marginTop: 30,
+        }}
+      >
+        <Text
+          style={{
+            color: "#555",
+            fontSize: 13,
+          }}
+        >
+          Bạn đã có tài khoản?
+        </Text>
+        <TouchableOpacity onPress={() => router.push("/auth/Login")}>
+          <Text
+            style={{
+              color: "#4169E1",
+              fontWeight: "bold",
+              fontSize: 13,
+            }}
+          >
+            {" "}
+            Đăng nhập
+          </Text>
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 }
