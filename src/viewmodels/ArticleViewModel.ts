b0e2412756c434031073451f2019fbd33f034c55
MinhPhucHCMUAF <22130215@st.hcmuaf.edu.vn>
2026-07-04 19:59:34 +0700

    tuan 5.ver.6 - update viewmodel article

--- src/viewmodels/ArticleViewModel.ts
+++ src/viewmodels/ArticleViewModel.ts
@@ -1,7 +1,24 @@
+import { useEffect, useState } from "react";
+import { Article } from "../models/Article";
 import { getArticles } from "../repository/ArticleRepository";
 
-export class ArticleViewModel {
-  async loadArticles() {
-    return await getArticles();
+export function useArticleViewModel() {
+  const [articles, setArticles] = useState<Article[]>([]);
+  const [loading, setLoading] = useState(false);
+  const loadArticles = async () => {
+    try{
+      setLoading(true)
+      const data = await getArticles();
+      setArticles(data);
+    }catch(error){
+      console.log(error)
+    } finally {
+      setLoading(false)
+    }
   }
+    useEffect(() => {
+          loadArticles();
+      }, []);
+
+      return {articles, loading, loadArticles};
 }
