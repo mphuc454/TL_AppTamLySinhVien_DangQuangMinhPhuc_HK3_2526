ccb87ca2f3ba88879ace33da9d95af35ea77a989
MinhPhucHCMUAF <22130215@st.hcmuaf.edu.vn>
2026-07-30 10:12:18 +0700

    tuan 9.ver.12 - add UI Admin chatbot

--- src/views/admin/ChatbotAdmin.tsx
+++ src/views/admin/ChatbotAdmin.tsx
@@ -1,9 +1,229 @@
-import { Text, View } from "react-native";
+import { Picker } from "@react-native-picker/picker";
+import {
+  ScrollView,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 export default function AdminChatbotView() {
   return (
-    <View style={{ flex: 1 }}>
-      <Text>Chatbot</Text>
-    </View>
+    <ScrollView
+      style={{ flex: 1, backgroundColor: "#F5F7FB" }}
+      contentContainerStyle={{ paddingBottom: 100 }}
+    >
+      <Text
+        style={{
+          fontSize: 26,
+          fontWeight: "bold",
+          marginBottom: 20,
+        }}
+      >
+        Quản lý Chatbot
+      </Text>
+
+      <View
+        style={{
+          backgroundColor: "#fff",
+          borderRadius: 15,
+          padding: 16,
+          marginBottom: 20,
+        }}
+      >
+        <Text
+          style={{
+            fontSize: 18,
+            fontWeight: "bold",
+            marginBottom: 15,
+          }}
+        >
+          Cấu hình Chatbot
+        </Text>
+
+        <Text>Chọn Model</Text>
+
+        <Picker
+          style={{
+            backgroundColor: "#e1bbbb",
+            borderRadius: 12,
+            borderWidth: 1,
+            height: 55,
+            paddingHorizontal: 16,
+            justifyContent: "space-between",
+            alignItems: "center",
+            flexDirection: "row",
+          }}
+          // selectedValue={gender}
+          // onValueChange={(value) => setGender(value)}
+        >
+          <Picker.Item
+            label="llama-3.1-8b-instant"
+            value="llama-3.1-8b-instant"
+          />
+          <Picker.Item
+            label="llama-3.3-70b-versatile"
+            value="llama-3.3-70b-versatile"
+          />
+          <Picker.Item
+            label="meta-llama/llama-prompt-guard-2-22m"
+            value="meta-llama/llama-prompt-guard-2-22m"
+          />
+          <Picker.Item
+            label="meta-llama/llama-prompt-guard-2-86m"
+            value="meta-llama/llama-prompt-guard-2-86m"
+          />
+          <Picker.Item
+            label="openai/gpt-oss-120b"
+            value="openai/gpt-oss-120b"
+          />
+        </Picker>
+        <View style={{ marginTop: 30 }}>
+          <Text>Temperature</Text>
+          <Picker
+            style={{
+              borderRadius: 12,
+              borderWidth: 1,
+              backgroundColor: "#f2dcdc",
+              height: 55,
+              paddingHorizontal: 16,
+              justifyContent: "space-between",
+              alignItems: "center",
+              flexDirection: "row",
+              marginBottom: 30,
+            }}
+            // selectedValue={gender}
+            // onValueChange={(value) => setGender(value)}
+          >
+            <Picker.Item label="0.3" value="0.3" />
+            <Picker.Item label="0.4" value="0.4" />
+            <Picker.Item label="0.5" value="0.5" />
+            <Picker.Item label="0.6" value="0.6" />
+            <Picker.Item label="0.7" value="0.7" />
+          </Picker>
+          <Text>Top P</Text>
+          <Picker
+            style={{
+              borderRadius: 12,
+              borderWidth: 1,
+              backgroundColor: "#f2dcdc",
+              height: 55,
+              paddingHorizontal: 16,
+              justifyContent: "space-between",
+              alignItems: "center",
+              flexDirection: "row",
+              marginBottom: 30,
+            }}
+            // selectedValue={gender}
+            // onValueChange={(value) => setGender(value)}
+          >
+            <Picker.Item label="0.8" value="0.8" />
+            <Picker.Item label="0.9" value="0.9" />
+            <Picker.Item label="1.0" value="1.0" />
+          </Picker>
+
+          <Text>Max Tokens</Text>
+
+          <Picker
+            style={{
+              borderRadius: 12,
+              borderWidth: 1,
+              backgroundColor: "#f2dcdc",
+              height: 55,
+              paddingHorizontal: 16,
+              justifyContent: "space-between",
+              alignItems: "center",
+              flexDirection: "row",
+              marginBottom: 30,
+            }}
+            // selectedValue={gender}
+            // onValueChange={(value) => setGender(value)}
+          >
+            <Picker.Item label="256" value="256" />
+            <Picker.Item label="512" value="512" />
+            <Picker.Item label="1024" value="1024" />
+          </Picker>
+        </View>
+
+        <TouchableOpacity
+          style={{
+            backgroundColor: "#2563EB",
+            marginTop: 20,
+            padding: 15,
+            borderRadius: 12,
+            alignItems: "center",
+          }}
+        >
+          <Text style={{ color: "#fff", fontWeight: "bold" }}>
+            Lưu cấu hình
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <View
+        style={{
+          backgroundColor: "#fff",
+          borderRadius: 15,
+          padding: 16,
+          marginBottom: 30,
+        }}
+      >
+        <Text
+          style={{
+            fontSize: 18,
+            fontWeight: "bold",
+            marginBottom: 15,
+          }}
+        >
+          Hỏi Chatbot
+        </Text>
+
+        <TextInput
+          placeholder="Ask chatbot..."
+          multiline
+          style={{
+            borderWidth: 1,
+            borderColor: "#ddd",
+            borderRadius: 12,
+            minHeight: 90,
+            padding: 12,
+            marginBottom: 15,
+          }}
+        />
+
+        <TouchableOpacity
+          style={{
+            backgroundColor: "#2563EB",
+            padding: 15,
+            borderRadius: 12,
+            alignItems: "center",
+          }}
+        >
+          <Text
+            style={{
+              color: "white",
+              fontWeight: "bold",
+            }}
+          >
+            Gửi
+          </Text>
+        </TouchableOpacity>
+
+        <View
+          style={{
+            borderWidth: 1,
+            borderColor: "#ddd",
+            borderRadius: 16,
+            minHeight: 90,
+            padding: 12,
+            marginTop: 15,
+          }}
+        >
+          <Text style={{ fontWeight: "bold" }}>Phản hồi Chatbot: </Text>
+
+          <Text style={{ marginTop: 10, color: "#666" }}></Text>
+        </View>
+      </View>
+    </ScrollView>
   );
 }
