63abdf7772fc8dc110cf1519e35e3d7b59f664d7
MinhPhucHCMUAF <22130215@st.hcmuaf.edu.vn>
2026-08-22 16:40:59 +0700

    tuan 12.ver.11 - fix bug contact viewmodel

--- src/viewmodels/ContactViewModel.ts
+++ src/viewmodels/ContactViewModel.ts
@@ -1,12 +1,25 @@
 import * as MailComposer from "expo-mail-composer";
 import { useState } from "react";
 import { Alert } from "react-native";
+import { checkHealthManagement } from "../repository/doctor/HealthManagementRepository";
 
 export function useSendMail() {
   const [loading, setLoading] = useState(false);
-  const sendContact = async (email: string) => {
+  const sendContact = async (id: number, email: string) => {
     try {
       setLoading(true);
+      const checkreq = await checkHealthManagement(id);
+      if (!checkreq) {
+        Alert.alert(
+          "Thông báo",
+          "Bạn cần gửi yêu cầu đến bác sĩ mới gửi email được.",
+        );
+        return false;
+      }
+      if (!checkreq.status) {
+        Alert.alert("Thông báo", "Bác sĩ chưa chấp nhận yêu cầu theo dõi.");
+        return false;
+      }
       const isAvailable = await MailComposer.isAvailableAsync();
       if (!isAvailable) {
         Alert.alert("Thông báo", "Thiết bị không hỗ trợ gửi email.");
